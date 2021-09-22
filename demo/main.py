4c940aa8d3a39c216ef3d0d06a171a94215a038a
yanshushuang <yanshushuang6@126.com>
2021-09-22 17:05:57 +0800

    第一个版本

---
+++ demo/main.py
@@ -0,0 +1,16 @@
+from alarm.alarm import DBUtil
+from handler.dingding import ding
+import json
+
+if __name__ == '__main__':
+    with open("config.json","r") as f:
+        config = json.load(f)
+    user = config.get("user")
+    passwd = config.get("passwd")
+    host = config.get("host")
+    port = config.get("port")
+    slaves = config.get("slaves")
+
+    with DBUtil(user=user, passwd=passwd, host=host, port=port, slaves=slaves,
+                alarm_handler=ding) as client:
+        client.run()
---
+++ handler/dingding.py
@@ -0,0 +1,61 @@
+import time
+import hmac
+import hashlib
+import base64
+import requests
+import urllib.parse
+import json
+from pathlib import Path
+
+file_path = Path(__file__).parent
+
+with open(file_path.joinpath("conf.json"), 'rb') as f:
+    conf = json.load(f)
+
+headers = {
+    'Content-type': 'application/json'
+}
+
+secret = conf.get("secret")
+url_prefix = conf.get("url")
+
+
+def ding(host, parameter, current, threshold, th_type):
+    timestamp = str(round(time.time() * 1000))
+    secret_enc = secret.encode('utf-8')
+    string_to_sign = '{}\n{}'.format(timestamp, secret)
+    string_to_sign_enc = string_to_sign.encode('utf-8')
+    hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    url = f"{url_prefix}&timestamp={timestamp}&sign={sign}"
+
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "title": "报警信息",
+            "text": f"#### 报警信息 \n > "
+                    f"##### Host:{host}\n >"
+                    f"##### Param:{parameter}\n >"
+                    f"##### Current Value:{current}\n >"
+                    f"##### Alarm Threshold: {th_type} {threshold}\n >"
+                    f"##### Time: {time.strftime('%m-%d %H:%M:%S')}\n >"
+            # "> ![screenshot](https://img.alicdn.com/tfs/TB1NwmBEL9TBuNjy1zbXXXpepXa-2400-1218.png)\n"
+
+        },
+        "at": {
+            # "atMobiles": [],
+            # "atUserIds": [],
+            "isAtAll": False
+        }
+    }
+
+    requests.post(url, data=json.dumps(data), headers=headers)
+
+
+if __name__ == '__main__':
+    host = "1.1.1.1"
+    parameter = "Table_open_cache_hits_precent"
+    current = "58.59"
+    threshold = "80"
+    th_type = "lt "
+    ding(host, parameter, current, threshold, th_type=th_type)
