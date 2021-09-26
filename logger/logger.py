0098c79e74c528a0fc595e203c10d33bfdc7f41d
yanshushuang <yanshushuang6@126.com>
2021-09-26 13:43:06 +0800

    抽离logger部分 用户能够自由选择handler

---
+++ logger/logger.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author:yanshushuang
+# datetime:2021/9/26
+import logging
+import time
+
+
+class Logger:
+    def __init__(self, stream=True, file=False):
+        self.logger = logging.getLogger("Alarm Logger")
+        self.formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
+        self.logger.setLevel(logging.INFO)
+        if stream:
+            self.__add_stream()
+        if file:
+            self.__add_file()
+
+    def __add_stream(self):
+        sh = logging.StreamHandler()
+        sh.setFormatter(self.formatter)
+        self.logger.addHandler(sh)
+
+    def __add_file(self):
+        filename = time.strftime("%Y-%m-%d")
+        fh = logging.FileHandler(filename=f"{filename}.log")
+        fh.setFormatter(self.formatter)
+        self.logger.addHandler(fh)
+
+    def get_logger(self):
+        return self.logger
+
+
+default_logger = Logger().get_logger()
