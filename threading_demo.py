e0dcf8599976d28415a911bccbbdaf894301e5e0
yanshushuang <yanshushuang6@126.com>
2021-10-09 15:49:42 +0800

    添加新参数检测

--- threading_demo.py
+++ threading_demo.py
@@ -7,7 +7,7 @@ import json
 from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 from alarm.engine import Alarmer
 from alarm.get_params import all_funcs
-from handler import dingding_handler, mail_handler
+from handler import dingding_handler, mail_handler,fake_handler
 from logger import Logger
 
 if __name__ == '__main__':
@@ -23,10 +23,11 @@ if __name__ == '__main__':
         port = config.get("port")
         slaves = config.get("slaves")
         tags = config.get("tags")
-        my_functions = ["disk", "thread_connect", "innodb_buffer_hint_precent"]
+        my_functions = ["disk", "thread_connect", "innodb_buffer_hint_precent",
+                        "trans_time", "innodb_buffer_pool_wait_free", "lock_waits"]
         my_func_dic = {name: all_funcs.get(name) for name in my_functions}
         client = Alarmer(user=user, passwd=passwd, host=host, port=port, slaves=slaves,
-                         alarm_handler=[mail_handler, dingding_handler], tags=tags, logger=my_logger,
+                         alarm_handler=[dingding_handler], tags=tags, logger=my_logger,
                          funcs=my_func_dic
                          )
         tasks.append(t.submit(client.th_run))
