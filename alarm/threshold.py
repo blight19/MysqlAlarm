9a81a228925ac3ca5c4c214f6feb8bd00302f016
yanshushuang <yanshushuang6@126.com>
2021-10-09 15:51:23 +0800

    添加参数

--- alarm/threshold.py
+++ alarm/threshold.py
@@ -5,17 +5,23 @@
 # 参数为报警参数
 CHECK_PARAMS = {
     "Size": {"type": "skip", "value": None},
-    "Threads_connected": {"type": "gt", "value": 80},
+    "Threads_connected": {"type": "gt", "value": 250},
     "Table_open_cache_hits_precent": {"type": "lt", "value": 80},
     "Innodb_buffer_hint_precent": {"type": "lt", "value": 80},
+    "Trans_time": {"type": "gt", "value": 60},
+    "Innodb_buffer_pool_wait_free": {"type": "gt", "value": 0},
+    "Lock_waits": {"type": "gt", "value": 0},
 }
 
 SLAVE_CHECK_PARAMS = {
     "Size": {"type": "skip", "value": None},
-    "Threads_connected": {"type": "gt", "value": 80},
+    "Threads_connected": {"type": "gt", "value": 250},
     "Table_open_cache_hits_precent": {"type": "lt", "value": 80},
     "Innodb_buffer_hint_precent": {"type": "lt", "value": 80},
+    "Trans_time": {"type": "gt", "value": 60},
+    "Innodb_buffer_pool_wait_free": {"type": "gt", "value": 0},
+    # "Lock_waits": {"type": "gt", "value": 0},#从节点不需要检测
     "Slave_IO_Running": {"type": "uneq", "value": 'Yes'},
     "Slave_SQL_Running": {"type": "uneq", "value": 'Yes'},
-    "Seconds_Behind_Master": {"type": "gt", "value": 20},
+    "Seconds_Behind_Master": {"type": "gt", "value": 60},
 }
